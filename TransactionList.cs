59a107505860e42066005eea500babfc4b7472b2
nordinzakaria <nordinzakaria@gmail.com>
2024-10-09 08:46:33 +0800

    Initial commit

---
+++ TransactionList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TransactionPackage
+{
+    public class TransactionList
+    {
+        // data 
+        private List<Transaction> transactions; // transactions is an Array object
+
+        // constructor -
+        // 1) no return type
+        // 2) name is that of the class
+        // 3) usually used for initialization
+        public TransactionList()
+        {
+            transactions = new List<Transaction>();
+        }
+
+        public TransactionList(int num)
+        {
+            transactions = new List<Transaction>(num);
+        }
+
+        // operations
+        public void Init(int num)
+        {
+            transactions = new List<Transaction>(num);
+        }
+
+        // how to complete this?????
+        public void Add(Transaction transaction)
+        {
+            transactions.Add(transaction);
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < transactions.Count; i++)
+            {
+                Console.WriteLine(transactions[i].getVal());
+            }
+        }
+    }
+}
